e2bd68fe22fb87aed295a26c7a02fe9a2590152d
juliagg14 <137561082+juliagg14@users.noreply.github.com>
2023-08-03 16:44:33 -0300

    atualização3

---
+++ exercicios/java.S/ex.04/java.js
@@ -0,0 +1,15 @@
+var texto = "javaScript";
+
+var numeroInteiro= 10;
+var numeroRacional= 10.25;
+
+
+var teste = true;
+
+
+alert(numeroRacional)
+
+
+document.write(texto)
+
+console.log(numeroInteiro)
---
+++ exercicios/java.S/ex.05/javaS.js
@@ -0,0 +1,6 @@
+var nome = prompt('digite o seu nome')
+var idade = prompt('digite sua idade')
+var idade2= 50
+console.log(idade)
+console.log(idade2)
+document.write('boa tarde ' + nome + ' tudo bem? sua idade e ' + idade + ' anos ' )
