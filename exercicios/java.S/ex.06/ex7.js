544e211811eec6438814d03f77d53baff84db8bc
juliagg14 <137561082+juliagg14@users.noreply.github.com>
2023-08-08 16:31:41 -0300

    atualização6

---
+++ exercicios/java.S/ex.06/ex7.js
@@ -0,0 +1,14 @@
+var parametro = prompt('digite um numero')
+
+switch(parseInt(parametro)){
+ case 1:
+    document.write('parametro 1')
+   break
+
+    case 2: 
+    document.write('parametro 2')
+    break
+
+    default:
+        document.write('defalt')
+}
